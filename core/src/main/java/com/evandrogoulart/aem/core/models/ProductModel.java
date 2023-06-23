5e78290536117e2986bc8ae3507627c22fe6be2d
evandro-goulart <evandro.ribeiro@webjump.com.br>
2023-06-23 15:28:09 -0300

    refactor: adds 2 more attributes to productModel component

--- core/src/main/java/com/evandrogoulart/aem/core/models/ProductModel.java
+++ core/src/main/java/com/evandrogoulart/aem/core/models/ProductModel.java
@@ -1,24 +1,25 @@
 package com.evandrogoulart.aem.core.models;
 
-import javax.annotation.PostConstruct;
-
+import lombok.Getter;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-
+@Getter
 @Model(adaptables = Resource.class)
 public class ProductModel {
-    @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
-    protected String text;
-    @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
-    protected String fileReference;
 
-    public String getText() {
-        return text;
-    }
-    public String getFileReference() {
-        return fileReference;
-    }
+    @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
+    private String name;
+    @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
+    private String category;
+    @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
+    private String price;
+    @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
+    private String fileReference;
+    @SlingObject
+    private Resource currentResource;
+
 }
